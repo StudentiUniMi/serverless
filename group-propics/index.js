9b7dc80d94b3ec547e7cc83c7f273f199e518b0e
Marco Aceti <mail@marcoaceti.it>
2021-02-16 22:15:54 +0100

    First commit

---
+++ group-propics/index.js
@@ -0,0 +1,39 @@
+addEventListener("fetch", event => {
+    event.respondWith(handleRequest(event.request));
+});
+
+const encodeGetParams = p =>
+    Object.entries(p).map(kv => kv.map(encodeURIComponent).join("=")).join("&");
+
+async function tgReq(path, params) {
+    let resp = await fetch("https://api.telegram.org/bot" + BOT_TOKEN + "/"
+        + path + "?" + encodeGetParams(params), {
+        method: "GET",
+        body: null
+    });
+    return await resp.json();
+}
+
+async function handleRequest(request) {
+    let cache = await caches.open("studentiunimi-profile-pictures");
+    let cachedResp = await cache.match(request);
+    if (cachedResp !== null && cachedResp !== undefined) {
+        return cachedResp;
+    }
+
+    const { searchParams } = new URL(request.url);
+    let chatID = searchParams.get("chat_id");
+    if (!chatID || chatID === "")
+        return new Response(JSON.stringify({ ok: false, error: "No chat_id specified" }), { status: 400 });
+
+    try {
+        let chatInfo = await tgReq("getChat", { "chat_id": chatID });
+        let file = await tgReq("getFile", { "file_id": chatInfo.result.photo.big_file_id });
+        let photo = await fetch("https://api.telegram.org/file/bot" + BOT_TOKEN + "/" + file.result.file_path);
+        let resp = new Response(photo.body, { status: 200 });
+        await cache.put(request, resp.clone());
+        return resp;
+    } catch (error) {
+        return new Response(JSON.stringify({ ok: false, error: "Bad chat_id" }), { status: 403 });
+    }
+}
